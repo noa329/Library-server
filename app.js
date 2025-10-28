0f34d883921497e812ad374b3e24cda85c2e1f55
Noa < >
2025-10-29 00:17:33 +0200

    Add basic REST API for book management: GET, POST, PUT, DELETE

---
+++ app.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import {borrows, books } from './db.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//נתיב ברירת מחדל
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = 5000;
+//הפעלת השרת
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+//קבלת כל הספרים
+app.get('/books', (req, res) => {
+  res.json(books);
+});
+//קבלת ספר לפי קוד
+app.get('/books/:id', (req, res) => {
+    const book=books.find(b=>b.id===+req.params.id);
+    if (book) {
+        res.json(book);
+    } else {
+        res.status(404).send('Book not found');
+    }
+});
+//הוספת ספר חדש
+app.post('/books', (req, res) => {
+    const newBook = req.body;
+    books.push(newBook);
+    res.json(books);
+});
+//עדכון ספר קיים
+app.put('/books/:id', (req, res) => {
+    const bookIndex = books.findIndex(b => b.id === +req.params.id);
+    if (bookIndex !== -1) {
+        books[bookIndex] = { ...books[bookIndex], ...req.body };
+        res.json(books[bookIndex]);
+    } else {
+        res.status(404).send('Book not found');
+    }
+});
+//מחיקת ספר
+app.delete('/books/:id', (req, res) => {
+    const bookIndex = books.findIndex(b => b.id === +req.params.id);
+    if (bookIndex !== -1) {
+        const deletedBook = books.splice(bookIndex, 1);
+        res.json(deletedBook);
+    } else {
+        res.status(404).send('Book not found');
+    }
+});
---
+++ db.js
@@ -0,0 +1,23 @@
+export const books= [
+    { id: 100, name: '21 bevit achad',category:'children', price: 50,isBorrowed: false },
+    { id: 101, name: 'holchet beshvila',category:'adults', price:65,isBorrowed: false },
+    { id: 102, name: '48 sheot',category:'adults', price:50,isBorrowed: true },
+    { id: 103, name: 'hanisher nechat',category:'drama', price:70,isBorrowed: false },
+    { id: 104, name: 'in the kitchen',category:'cooking', price:50,isBorrowed: false },
+];
+
+export const borrows = [
+  {
+    userId: 501,
+    borrowDate: '2025-10-20'
+  },
+  {
+    userId: 502,
+    borrowDate: '2025-09-15'
+  },
+  {
+    userId: 503,
+    borrowDate: '2025-10-03'
+  },
+
+];
